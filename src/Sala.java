2187205249582eff824d1c5657662aa538b4466e
Luca Tsiklauri <ltsik@myuax.com>
2026-02-23 20:37:37 +0100

    Creacion de Sala

---
+++ src/Sala.java
@@ -0,0 +1,23 @@
+public class Sala {
+    private int codigoSala;
+    private String descripcion;
+
+    public Sala(int codigoSala, String descripcion) {
+        this.codigoSala = codigoSala;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigoSala() {
+        return codigoSala;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String toString(){
+        return "Codigo de sala: " + codigoSala + ", Descripcion: " + descripcion;
+    }
+
+
+}
