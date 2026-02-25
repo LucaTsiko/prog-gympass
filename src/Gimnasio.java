b99d48b348d062485e7cd15dcb3ad52c949a9431
Luca Tsiklauri <ltsik@myuax.com>
2026-02-25 08:44:50 +0100

    Fix: (Gimnasio); corregido error en la condición del metodo registrarSocio

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -5,17 +5,46 @@ public class Gimnasio {
     private Sala salas[];
     private int responsableId;
 
-    private final int Cantidad_Socios;
-    private final int Cantidad_Salas;
+    private final int cantidad_Socios;
+    private final int cantidad_Salas;
 
     public Gimnasio(String codigo, String nombre,int CantidadSocios, int cantidadSalas, int responsableId) {
         this.codigo = codigo;
         this.nombre = nombre;
 
-        this.Cantidad_Socios = CantidadSocios;
-        this.Cantidad_Salas = cantidadSalas;
+        this.cantidad_Socios = CantidadSocios;
+        this.cantidad_Salas = cantidadSalas;
 
         this.responsableId = responsableId;
 
+    }
+
+    /**
+     * Metodo para devolver una posicion libre
+     * @return numero de la posicion libre si hay. Si no hay devuelve -1
+     */
+    private int devolverPosicionLibre(){
+        int posicionLibre = -1;
+
+        for (int i = 0; i < cantidad_Socios; i++) {
+            if(socios[i] == null){
+                posicionLibre = i;
+            }
+        }
+        return posicionLibre;
+    }
+
+    private boolean resigtrarSocio(Socio s){
+        boolean socioRegistrado = false;
+        int posicionLibre = devolverPosicionLibre();
+
+        for (int i = 0; i < cantidad_Socios && !socioRegistrado; i++) {
+            if(posicionLibre >= 0 && s.getNumSocio() != socios[i].getNumSocio()){
+                socios[i] = s;
+                socioRegistrado = true;
+
+            }
+        }
+        return socioRegistrado;
     }
 }
